9871f62656d15be949e7697d8f5c85534e54c3d2
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-01-21 10:06:31 +0800

    Working Scrolling View + Leaving Room

--- Red VS Blue/GameSelectionViewController.swift
+++ Red VS Blue/GameSelectionViewController.swift
@@ -16,7 +16,7 @@ class GameSelectionViewController: UIViewController {
     @IBOutlet weak var clientNameLabel: UILabel!
     @IBOutlet weak var clientBioLabel: UILabel!
     
-    @IBOutlet weak var gamesScrollView: UIView!
+    @IBOutlet weak var gameScrollView: UIView!
     @IBOutlet weak var testButton: UIButton!
     
     var roomRef: DocumentReference!
@@ -36,6 +36,7 @@ class GameSelectionViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        roomListener.remove()
     }
     
     func startListening() {
@@ -47,6 +48,19 @@ class GameSelectionViewController: UIViewController {
                 self.hostNameLabel.text = documentSnapshot.data()!["hostUserName"] as? String
                 let hostBio = documentSnapshot.data()!["hostUserBio"] as? String
                 self.hostBioLabel.text = hostBio == "" ? "This player has no bio." : hostBio
+                if let _ = documentSnapshot.data()!["endGameRequest"] as? Bool {
+                    let alertController = UIAlertController(title: "Game Ended",
+                                                            message: "The other player has left!",
+                                                            preferredStyle: .alert)
+
+                    alertController.addAction(UIAlertAction(title: "OK",
+                                                            style: .default)
+                    { (action) in
+                        self.deleteRoomAndLeave()
+                    })
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
             } else {
                 print("Error getting room data \(error!)")
                 return
@@ -55,16 +69,23 @@ class GameSelectionViewController: UIViewController {
     }
     
     func loadGameButtons() {
-        var x = 20
+        let viewWidth = gameScrollView.frame.size.width
+        let horizontalGap = 10, verticalGap = 20, iconWidth = 80, startingXPos = 20, startingYPos = 20
+        var x = startingXPos, y = startingYPos
+        //gameScrollView.frame.size.height = CGFloat(numRows * (verticalGap + iconWidth))
+        
         for game in GameCollection.singleton.games {
             let button = UIButton(type: .custom) as UIButton
-            button.frame = CGRect(x: x, y: 20, width: 75, height: 75)
+            if x + horizontalGap + iconWidth >= Int(viewWidth) {
+                y += verticalGap + iconWidth
+                x = startingXPos
+            }
+            button.frame = CGRect(x: x, y: y, width: iconWidth, height: iconWidth)
             button.setImage(game.gameIconImage, for: .normal)
             //button.addTarget(self, action: "btnTouched:", forControlEvents:.TouchUpInside)
-            gamesScrollView.addSubview(button)
-            x = x + 20
+            gameScrollView.addSubview(button)
+            x += horizontalGap + iconWidth
         }
-        
     }
     
     @IBAction func pressedGoButton(_ sender: Any) {
@@ -72,5 +93,29 @@ class GameSelectionViewController: UIViewController {
     
     
     @IBAction func pressedLeaveButton(_ sender: Any) {
+        let alertController = UIAlertController(title: nil,
+                                                message: "Are you sure you want to leave the game?",
+                                                preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Cancel",
+                                                style: .cancel,
+                                                handler: nil))
+        alertController.addAction(UIAlertAction(title: "Confirm",
+                                                style: .default)
+        { (action) in
+            //self.roomRef.delete()
+            self.roomRef.updateData([
+                "endGameRequest": true
+            ])
+            self.navigationController?.popToRootViewController(animated: true)
+            // TODO: Game Result Page
+        })
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func deleteRoomAndLeave() {
+        roomRef.delete()
+        navigationController?.popToRootViewController(animated: true)
     }
 }
