baaeebfc17bce7852b5c65be338c3d355789adf7
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-02-04 21:19:33 +0800

    Hangman layout save

--- Red VS Blue/Controllers/HangmanViewController.swift
+++ Red VS Blue/Controllers/HangmanViewController.swift
@@ -29,6 +29,8 @@ class HangmanViewController: UIViewController {
     let roomId = RoomStatusStorage.shared.roomId
     let score = RoomStatusStorage.shared.score
     
+    var word: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(true, animated: false)
@@ -40,9 +42,52 @@ class HangmanViewController: UIViewController {
         
         self.upperBannerView.backgroundColor = isHost ? UIColor.blue: UIColor.red
         self.lowerBannerView.backgroundColor = isHost ? UIColor.red: UIColor.blue
+        
+        //removeAllArrangedSubviews(stackView: letterStatusStackView)
+        
+        word = RandomStringGenerator.shared.generateRandomHangmanWord()
+        
+        for _ in 0..<word.count {
+            letterStatusStackView.addArrangedSubview(generateOneLetterStackView(fontSize: 30))
+        }
+        for index in 0..<letterStatusStackView.arrangedSubviews.count {
+            changeStackViewLabel(stackView: letterStatusStackView.arrangedSubviews[index] as! UIStackView, letter: String(Array(word)[index]))
+        }
     }
     
     @IBAction func pressedLetterButton(_ sender: Any) {
+        
+    }
+    
+    func removeAllArrangedSubviews(stackView: UIStackView) {
+        for view in stackView.arrangedSubviews {
+            stackView.removeArrangedSubview(view)
+        }
+    }
+    
+    func generateOneLetterStackView(fontSize: CGFloat) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill // .leading .firstBaseline .center .trailing .lastBaseline
+        stackView.distribution = .fill // .fillEqually .fillProportionally .equalSpacing .equalCentering
+        stackView.spacing = 1
+        
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: fontSize)
+        label.text = "K"
+        label.textAlignment = .center
+        stackView.addArrangedSubview(label)
+        
+        let underscoreLabel = UILabel()
+        underscoreLabel.text = "‾‾‾"
+        underscoreLabel.textColor = UIColor.black
+        underscoreLabel.font = UIFont.boldSystemFont(ofSize: fontSize)
+        stackView.addArrangedSubview(underscoreLabel)
+        
+        return stackView
+    }
+    
+    func changeStackViewLabel(stackView: UIStackView, letter: String) {
         
     }
 }
