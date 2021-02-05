f342b122a2c1c9e575ab670c13b8a8673f58c346
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-02-05 21:21:24 +0800

    Changed Back Initial View Controller

--- Red VS Blue/GameCollection/GameCollection.swift
+++ Red VS Blue/GameCollection/GameCollection.swift
@@ -14,7 +14,7 @@ class GameCollection {
     var games: [Game]
     
     private init() {
-        games = [TicTacToeGame(), HangmanGame(), CountTo21Game(), NumberPuzzleGame(), RandomGame()]
+        games = [TicTacToeGame(), CountTo21Game(), NumberPuzzleGame(), RandomGame()]
         //games = [TicTacToeGame(), RandomGame()]
     }
 }
